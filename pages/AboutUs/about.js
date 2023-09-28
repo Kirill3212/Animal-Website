23e5eb8eca0e3e385c25e5aa641ee0142f00deff
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2023-09-28 20:55:44 +0300

    Adding Achivments carusel and styling

--- pages/AboutUs/about.js
+++ pages/AboutUs/about.js
@@ -1,3 +1,64 @@
+// Achievements Section
+const prevAchievementBtn = document.querySelector(".ach-prev-btn");
+const nextAchievementBtn = document.querySelector(".ach-next-btn");
+const achievementsSlidesImg = Array.from(
+  document.querySelectorAll(".achievements-column")
+);
+console.log(achievementsSlidesImg);
+
+achievementsSlidesImg.forEach((slideImg, index) => {
+  slideImg.addEventListener("click", () => {
+    currentAchievementSlide(index + 1);
+  });
+});
+
+let slideAchievementIndex = 1;
+showAchievementSlides(slideAchievementIndex);
+
+prevAchievementBtn.addEventListener("click", function () {
+  slideAchievementIndex -= 1;
+  showAchievementSlides(slideAchievementIndex);
+});
+
+nextAchievementBtn.addEventListener("click", function () {
+  slideAchievementIndex += 1;
+  showAchievementSlides(slideAchievementIndex);
+});
+
+function currentAchievementSlide(n) {
+  showAchievementSlides((slideAchievementIndex = n));
+}
+
+function showAchievementSlides(n) {
+  let i;
+  let achievementSlides = document.getElementsByClassName("achievements-slide");
+  let achievementDots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+
+  if (n > achievementSlides.length) {
+    slideAchievementIndex = 1;
+  }
+
+  if (n < 1) {
+    slideAchievementIndex = achievementSlides.length;
+  }
+
+  for (i = 0; i < achievementSlides.length; i++) {
+    achievementSlides[i].style.display = "none";
+  }
+
+  for (i = 0; i < achievementDots.length; i++) {
+    achievementDots[i].className = achievementDots[i].className.replace(
+      " active",
+      ""
+    );
+  }
+
+  achievementSlides[slideAchievementIndex - 1].style.display = "block";
+  achievementDots[slideAchievementIndex - 1].className += " active";
+  captionText.innerHTML = achievementDots[slideAchievementIndex - 1].alt;
+}
+
 // Scroll Section
 const scrollImgs = Array.from(document.querySelectorAll(".scroll-img"));
 const middleElements = Array.from(document.querySelectorAll(".middle"));
